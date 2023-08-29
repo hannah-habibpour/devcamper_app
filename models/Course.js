0ef54749179cbfd53799b46ff42965c35f025e00
Hannah-Habibpour <hana.habibpor@gmail.com>
2023-08-29 17:38:48 -0400

    Calculating average cost with aggregate

--- models/Course.js
+++ models/Course.js
@@ -1,3 +1,4 @@
+const e = require('express');
 const mongoose = require('mongoose');
 
 const CourseSchema = new mongoose.Schema({
@@ -36,6 +37,40 @@ const CourseSchema = new mongoose.Schema({
         ref: 'Bootcamp',
         required: true
     }
+});
+
+CourseSchema.statics.getAverageCost = async function (bootcampId) {
+    console.log('Calculating avg cost...');
+
+    const obj = await this.aggregate([
+        {
+            $match: { bootcamp: bootcampId }
+        },
+        {
+            $group: {
+                _id: '$bootcamp',
+                averageCost: { $avg: '$tuition' }
+            }
+        }
+    ]);
+
+    try {
+        await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+            averageCost: Math.ceil(obj[0].averageCost / 10) * 10
+        })
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+// Call getAverageCost after save
+CourseSchema.post('save', function () {
+    this.constructor.getAverageCost(this.bootcamp);
+});
+
+// Call getAverageCost before remove
+CourseSchema.pre('remove', function () {
+    this.constructor.getAverageCost(this.bootcamp);
 });
 
 module.exports = mongoose.model('Course', CourseSchema);
