425c76349b4262dc1e2021ebbfe4ca6b1f4aec5b
Hannah-Habibpour <hana.habibpor@gmail.com>
2023-08-30 11:28:17 -0400

    Created advanced results middleware

--- models/Course.js
+++ models/Course.js
@@ -40,8 +40,6 @@ const CourseSchema = new mongoose.Schema({
 });
 
 CourseSchema.statics.getAverageCost = async function (bootcampId) {
-    console.log('Calculating avg cost...');
-
     const obj = await this.aggregate([
         {
             $match: { bootcamp: bootcampId }
--- seeder.js
+++ seeder.js
@@ -21,7 +21,7 @@ const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`, 'u
 const importData = async () => {
     try {
         await Bootcamp.create(Bootcamps);
-        // await Course.create(courses);
+        await Course.create(courses);
         console.log('Data Imported...');
         process.exit();
     } catch (error) {
