87b1a5c52e84f2c7a13d9807877697ccd804533d
Hannah-Habibpour <hana.habibpor@gmail.com>
2023-09-01 12:10:56 -0400

    Sending JWT in a Cookie

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const dontenv = require('dotenv');
 const morgan = require('morgan');
 const colors = require('colors');
 const fileupload = require('express-fileupload');
+const cookieParser = require('cookie-parser');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
-
 
 //Load env vars
 dontenv.config({ path: './config/config.env' });
@@ -23,6 +23,9 @@ const app = express();
 
 // Body parser
 app.use(express.json());
+
+// Cookie parser
+app.use(cookieParser());
 
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
