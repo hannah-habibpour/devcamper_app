6446b57f2864ee6ceb0a5862f024afd6db6f19ff
Hannah-Habibpour <hana.habibpor@gmail.com>
2023-09-01 14:17:21 -0400

    Created Auth Protect Middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -69,4 +69,16 @@ const sendTokenRespone = (user, statusCode, res) => {
             success: true,
             token
         });
-}
+};
+
+// @desc    Get current logged in user
+// @route   POST /api/v1/auth/me
+// @access  Private
+exports.getMe = asyncHandler(async (req, res, next) => {
+    const user = await User.findById(req.user.id);
+
+    res.status(200).json({
+        success: true,
+        data: user
+    });
+});
--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -17,6 +17,8 @@ const courseRouter = require('./courses');
 
 const router = express.Router();
 
+const { protect } = require('../middleware/auth');
+
 // Re-route into other resource routers
 router.use('/:bootcampId/courses', courseRouter);
 
@@ -24,17 +26,17 @@ router
     .route('/radius/:zipcode/:distance')
     .get(getBootcampsInRadius);
 
-router.route('/:id/photo').put(bootcampPhotoUpload);
+router.route('/:id/photo').put(protect, bootcampPhotoUpload);
 
 router
     .route('/')
     .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
-    .post(createBootcamp);
+    .post(protect, createBootcamp);
 
 router
     .route('/:id')
     .get(getBootcamp)
-    .put(updateBootcamp)
-    .delete(deleteBootcamp);
+    .put(protect, updateBootcamp)
+    .delete(protect, deleteBootcamp);
 
 module.exports = router;
