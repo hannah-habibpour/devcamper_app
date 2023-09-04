18bd4567f753e3f53d60e50c03d88d669d7e9354
Hannah-Habibpour <hana.habibpor@gmail.com>
2023-09-04 09:56:42 -0400

    Added Role Authorization

--- routes/courses.js
+++ routes/courses.js
@@ -11,7 +11,7 @@ const Course = require('../models/Course');
 const advancedResults = require('../middleware/advancedResults');
 const router = express.Router({mergeParams: true});
 
-const { protect } = require('../middleware/auth');
+const { protect, authorize } = require('../middleware/auth');
 
 router
     .route('/')
@@ -19,11 +19,11 @@ router
             path: 'bootcamp',
             select: 'name description'
         }), getCourses)
-    .post(protect, addCourse);
+    .post(protect, authorize('publisher', 'admin'), addCourse);
 router
     .route('/:id')
     .get(getCourse)
-    .put(protect, updateCourse)
-    .delete(protect, deleteCourse);
+    .put(protect, authorize('publisher', 'admin'), updateCourse)
+    .delete(protect, authorize('publisher', 'admin'), deleteCourse);
 
 module.exports = router;
