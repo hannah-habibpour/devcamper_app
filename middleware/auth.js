62236aa74f98f5df8ff571811d19b813d7c8f1cb
Hannah-Habibpour <hana.habibpor@gmail.com>
2023-09-07 11:38:40 -0400

    Added Logout to clear token cookie route

--- middleware/auth.js
+++ middleware/auth.js
@@ -11,16 +11,17 @@ exports.protect = asyncHandler(async (req, res, next) => {
         req.headers.authorization &&
         req.headers.authorization.startsWith('Bearer')
     ) {
+        // Set token from Bearer token in header
         token = req.headers.authorization.split(' ')[1];
+        // Set token from cookie
+    } else if (req.cookies.token) {
+        token = req.cookies.token
     }
 
-    // else if (req.cookies.token) {
-    //     token = req.cookies.token
-    // }
 
     // Make sure token exists
     if (!token) {
-        return next(new ErrorResponse('Not authorize to access this route', 401));
+        return next(new ErrorResponse('Not authorized to access this route', 401));
     }
 
     try {
@@ -30,7 +31,7 @@ exports.protect = asyncHandler(async (req, res, next) => {
         req.user = await User.findById(decoded.id);
         next();
     } catch (err) {
-        return next(new ErrorResponse('Not authorize to access this route', 401));
+        return next(new ErrorResponse('Not authorized to access this route', 401));
     }
 });
 
